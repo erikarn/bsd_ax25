64f2d9586722c2de3e7d2df92f6eaec46d2a1153
Adrian Chadd <adrian@freebsd.org>
2017-10-24 21:18:26 -0700

    Begn fleshing out an APRS igate client.
    This will take some time as I'm missing async dns server support to
    resolve DNS hosts.  Fun times, fun times.

---
+++ src/proto_aprs_igate.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <string.h>
+
+#include <sys/queue.h>
+#include <netinet/in.h>
+
+#include <event2/event.h>
+
+#include "buf.h"
+#include "buf_list.h"
+#include "conn.h"
+#include "proto_aprs_igate.h"
+
+/*
+ * This implements a simple APRS I-Gate client.
+ */
+
+struct proto_aprs_igate *
+proto_aprs_igate_create(struct event_base *eb)
+{
+	struct proto_aprs_igate *k;
+
+	k = calloc(1, sizeof(*k));
+	if (k == NULL) {
+		warn("%s: calloc", __func__);
+		goto err;
+	}
+
+	/* XXX TODO: may want to make this configurable */
+	k->rx_buf = buf_create(32768);
+	if (k->rx_buf == NULL) {
+		warn("%s: buf", __func__);
+		goto err;
+	}
+
+	k->eb = eb;
+	return (k);
+
+err:
+	if (k->conn)
+		conn_free(k->conn);
+	if (k->host)
+		free(k->host);
+	if (k->rx_buf)
+		buf_free(k->rx_buf);
+	free(k);
+	return (NULL);
+}
+
+void
+proto_aprs_igate_free(struct proto_aprs_igate *k)
+{
+	if (k->conn)
+		conn_close(k->conn);
+	if (k->host)
+		free(k->host);
+	if (k->rx_buf)
+		buf_free(k->rx_buf);
+	free(k);
+}
+
+int
+proto_aprs_igate_set_host(struct proto_aprs_igate *k, const char *host, int port)
+{
+	if (k->host != NULL)
+		free(k->host);
+	k->host = strdup(host);
+	k->port = port;
+
+	return (0);
+}
+
+static int
+proto_aprs_igate_read_cb(struct conn *c, void *arg, char *buf, int len, int xerrno)
+{
+	int i;
+
+	fprintf(stderr, "%s: called\n", __func__);
+
+	/* XXX TODO: notify owner */
+	if (len == 0) {
+		fprintf(stderr, "%s: conn is EOF\n", __func__);
+		return (0);
+	}
+
+	/* XXX TODO: notify owner */
+	if (len < 0) {
+		fprintf(stderr, "%s: conn read fail, errno+%d\n", __func__,
+		    xerrno);
+		return (0);
+	}
+
+
+	fprintf(stderr, "%s: read %d bytes\n", __func__, len);
+
+	for (i = 0; i < len; i++) {
+		if (i % 16 == 0)
+			fprintf(stderr, "0x%.4x: ", i);
+		fprintf(stderr, "%.2x ", buf[i] & 0xff);
+		if (i % 16 == 15)
+			fprintf(stderr, "\n");
+	}
+	fprintf(stderr, "\n");
+
+	return (0);
+}
+
+static int
+proto_aprs_igate_write_cb(struct conn *c, void *arg, int xerrno)
+{
+
+	fprintf(stderr, "%s: called\n", __func__);
+	return (0);
+}
+
+static int
+proto_aprs_igate_connect_cb(struct conn *c, void *arg, int xerrno)
+{
+
+	fprintf(stderr, "%s: called\n", __func__);
+	return (0);
+}
+
+static int
+proto_aprs_igate_close_cb(struct conn *c, void *arg, int xerrno)
+{
+
+	fprintf(stderr, "%s: called\n", __func__);
+	return (0);
+}
+
+int
+proto_aprs_igate_connect(struct proto_aprs_igate *k)
+{
+	struct sockaddr_storage lcl, peer;
+	struct sockaddr_in *ls, *ps;
+
+	if (k->host == NULL || k->port == 0) {
+		fprintf(stderr, "%s: no host/port set\n", __func__);
+		return (-1);
+	}
+
+	if (k->conn != NULL) {
+		fprintf(stderr, "%s: already connected\n", __func__);
+		return (-1);
+	}
+
+	/* Create connection */
+	k->conn = conn_create(k->eb);
+	if (k->conn == NULL) {
+		fprintf(stderr, "%s: failed to create conn\n", __func__);
+		return (-1);
+	}
+
+	/* Setup information for outbound connection - for now, assume localhost:8001 */
+	bzero(&lcl, sizeof(lcl));
+	bzero(&peer, sizeof(peer));
+	ls = (void *) &lcl;
+	ps = (void *) &peer;
+
+	/* XXX TODO: start the DNS connection */
+
+	(void) conn_set_lcl(k->conn, (void *) ls);
+	(void) conn_set_peer(k->conn, (void *) ps);
+
+	/*
+	 * For now, manually setup the callbacks required for
+	 * interface lifecycle management.
+	 */
+	k->conn->cb.cbdata = k;
+	k->conn->cb.read_cb = proto_aprs_igate_read_cb;
+	k->conn->cb.write_cb = proto_aprs_igate_write_cb;
+	k->conn->cb.connect_cb = proto_aprs_igate_connect_cb;
+	k->conn->cb.close_cb = proto_aprs_igate_close_cb;
+
+	/*
+	 * Start the connect.  If it succeeds here then we
+	 * wait for notification that we actually have succeeded
+	 * before continuing.
+	 */
+	if (conn_setup(k->conn) != 0) {
+		fprintf(stderr, "%s: conn_setup failed\n", __func__);
+		return (-1);
+	}
+
+	if (conn_connect(k->conn) != 0) {
+		fprintf(stderr, "%s: conn_connect failed\n", __func__);
+		return (-1);
+	}
+
+	/* waiting for connect notification now */
+	k->state = PROTO_KISS_CONN_CONNECTING;
+
+	return (0);
+}
+
+int
+proto_aprs_igate_disconnect(struct proto_aprs_igate *k)
+{
+
+	if (k->conn == NULL) {
+		fprintf(stderr, "%s: called without being connected\n", __func__);
+		return (-1);
+	}
+
+	/* Close, free the connection; we're done */
+	conn_close(k->conn);
+	conn_free(k->conn);
+	k->conn = NULL;
+
+	/* Idle state, need to be reconfigured */
+	k->state = PROTO_KISS_CONN_IDLE;
+
+	return (0);
+}
---
+++ src/proto_aprs_igate.h
@@ -0,0 +1,37 @@
+#ifndef	__PROTO_APRS_IGATE_H__
+#define	__PROTO_APRS_IGATE_H__
+
+typedef enum {
+	PROTO_KISS_CONN_NONE,
+	PROTO_KISS_CONN_IDLE,
+	PROTO_KISS_CONN_CONNECTING,
+	PROTO_KISS_CONN_ACTIVE,
+	PROTO_KISS_CONN_CLOSING,
+} proto_aprs_igate_state_t;
+
+struct proto_aprs_igate {
+	struct event_base *eb;
+	struct conn *conn;
+
+	proto_aprs_igate_state_t state;
+
+	char *login;
+	char *password;
+
+	char *host;
+	int port;
+
+	struct {
+		float filt_lat, filt_long;
+		float filt_range;
+	} filter_settings;
+
+	struct buf *rx_buf;
+};
+
+extern	struct proto_aprs_igate * proto_aprs_igate_create(struct event_base *eb);
+extern	int proto_aprs_igate_set_login(struct proto_aprs_igate *, char *login, char *pass);
+extern	int proto_aprs_igate_connect(struct proto_aprs_igate *);
+extern	int proto_aprs_igate_disconnect(struct proto_aprs_igate *);
+
+#endif	/* __PROTO_APRS_IGATE_H__ */
