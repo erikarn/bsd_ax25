c2fd91b63a3d0fa45aae840a6095fb717d437d63
Adrian Chadd <adrian@freebsd.org>
2017-11-06 23:03:08 -0800

    Add some basic AX.25 packet decoding hacks. nothing actual dcoding; it's all printing for now.

--- src/ax25.c
+++ src/ax25.c
@@ -1,10 +1,64 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "ax25.h"
 #include "kiss.h"
 #include "ax25_pkt_examples.h"
+
+/*
+ * Print an ax.25 address field - callsign, ssid field, C/H bits.
+ *
+ * return 1 if there's another address following, 0 if this is the
+ * final address field.  -1 if there's an error.
+ */
+static int
+ax25_pkt_address_parse(const uint8_t *buf, int len)
+{
+	char callsign[8];
+	int i;
+
+	if (len < 7)
+		return (-1);
+
+	memset(callsign, 0, sizeof(callsign));
+
+	for (i = 0; i < 6; i++) {
+		callsign[i] = buf[i] >> 1;
+		/* we shouldn't get an end of address here */
+		if (buf[i] & 0x1)
+			return (-1);
+	}
+
+	printf("%s-%d", callsign, buf[7] & 0xf);
+
+	return ((buf[7] & 0x01) == 0);
+}
+
+static int
+ax25_pkt_control_parse(uint8_t ctrl)
+{
+	switch (ctrl & 0x3) {
+		case 0:		/* low bit is 0, rest is i-frame */
+		case 2:
+			printf("I-frame; N(S)=%d, P=%d, N(R)=%d",
+			    (ctrl & 0xe) >> 1,
+			    (ctrl & 0x10) >> 4,
+			    (ctrl & 0xe0) >> 5);
+			break;
+		case 1:		/* S-frame */
+			printf("S-frame; S=%d, P/F=%d, N(R)=%d",
+			    (ctrl & 0xc) >> 2,
+			    (ctrl & 0x10) >> 4,
+			    (ctrl & 0xe0) >> 5);
+			break;
+		case 3:		// U-frame */
+			printf("U-frame: M-field: 0x%.2x", ctrl);
+			break;
+	}
+	return (0);
+}
 
 /*
  * Parse an AX.25 payload.
@@ -12,7 +66,8 @@
 int
 ax25_pkt_parse(const uint8_t *buf, int len)
 {
-	int i;
+	int i, r;
+	uint8_t ctrl, pid;
 	i = 0;
 
 	printf("%s: total length: %d\n", __func__, len);
@@ -23,17 +78,48 @@ ax25_pkt_parse(const uint8_t *buf, int len)
 	 * through a repeater.
 	 */
 	do {
-		printf("[%.2x) %c]",
-		    (buf[i] & 0xff),
-		    (buf[i] & 0xff) >> 1);
-	} while ((buf[i++] & 0x1) == 0);
-	printf("\n");
+		r = ax25_pkt_address_parse(buf + i, len - i);
+		printf(" ");
+		i = i + 7;
+		if (r < 1)
+			break;
+	} while (i < len);
+	printf(": ");
+	if (i >= len) {
+		goto end;
+	}
 
 	/* Following byte is the control byte */
+	ax25_pkt_control_parse(buf[i]);
+	ctrl = buf[i];
+	i++;
+
+	printf(": ");
+
+	/* XXX hack, yes, for i-frames and payload */
+	if (((buf[i] & 0x3) == 0) || ((buf[i] & 0x3) == 2)) {
+		if (i >= len)
+			goto end;
+
+		pid = buf[i];
+		i++;
+
+		printf("PID: 0x%.2x: ", pid);
+		if (i >= len)
+			goto end;
+
+		printf("%.*s", i, &buf[i]);
+	}
+
+	/* PID, info, etc if appropriate */
+
+end:
+	printf("\n");
 
 	return (0);
 }
 
+#ifdef STANDALONE
 int
 main(int argc, const char *argv[])
 {
@@ -44,3 +130,4 @@ main(int argc, const char *argv[])
 	    kbuf, &kbuf_len);
 	ax25_pkt_parse(kbuf, kbuf_len);
 }
+#endif
--- src/ax25.h
+++ src/ax25.h
@@ -1,11 +1,24 @@
 #ifndef	__AX25_PKT_H__
 #define	__AX25_PKT_H__
 
+struct pkt_ax25 {
+	/* This is the original packet payload; yes should be buf_t */
+	char *buf;
+	int len;
 
+	/* Offset inside the buffer for the beginning of things */
+	int da_offset; /* offset for destination address */
+	int sa_offset; /* offset for source address (single, list, etc) */
+	int ctrl_offset;	/* offset of control field */
 
-/*
- * The AX.25 packet header has a number of options.
- */
+	int pid_offset;		/* offset of PID field, or -1 */
+	int info_offset;	/* offset for start of info, or -1 */
+
+	/* other things */
+	int num_srcs_addresses;	/* how many source addresses are there */
+};
+
+extern	int ax25_pkt_parse(const uint8_t *buf, int len);
 
 #endif	/* __AX25_PKT_H__ */
 
