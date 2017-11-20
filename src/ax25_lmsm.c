ddc380948ca1a37702d64d7a45acb7258acba45f
Adrian Chadd <adrian@freebsd.org>
2017-11-19 21:56:33 -0800

    And sketch the methods I have to implement for lmsm

---
+++ src/ax25_lmsm.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+ * This implements the Link Multiplexer State Machine.
+ *
+ * This is the interface between the physical state machine and
+ * the upper layer Data Link State Machine.
+ */
+
+/*
+ * For BSD-AX25 this is a generic interface for queuing and dequeuing
+ * AX.25 frames from a TNC (eg a KISS TNC) which doesn't provide a full
+ * AX.25 stack.
+ */
+
+/* PH-SEIZE request */
+
+/* PH-SEIZE confirm */
+
+/* PH-RELEASE request */
+
+/* PH-EXPEDITED-DATA request */
+
+/* PH-DATA request */
+
+/* PH-DATA indication */
---
+++ src/ax25_lmsm.h
@@ -0,0 +1,9 @@
+#ifndef	__AX25_LMSM_H__
+#define	__AX25_LMSM_H__
+
+
+struct ax25_lmsm {
+	struct ebase *eb;
+};
+
+#endif	/* __AX25_LMSM_H__ */
