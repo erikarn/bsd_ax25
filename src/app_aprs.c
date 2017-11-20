e259db1a77a05709c94a910eb5712bd87346b0ab
Adrian Chadd <adrian@freebsd.org>
2017-11-19 21:56:58 -0800

    .. and this too

---
+++ src/app_aprs.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/*
+ * This is an APRS application.
+ */
---
+++ src/app_aprs.h
@@ -0,0 +1,13 @@
+#ifndef	__APP_APRS_H__
+#define	__APP_APRS_H__
+
+/*
+ * An APRS application instance sends and receives APRS
+ * messages to its designated endpoints, including handling
+ * periodic events such as beaconing, sending messages, etc.
+ *
+ * It has a list of interfaces to potentially send/receive
+ * APRS frames on.
+ */
+
+#endif	/* __APP_APRS_H__ */
