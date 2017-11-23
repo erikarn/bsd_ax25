423fbe31645186a9fa22a7844e374c146000a0ae
Adrian Chadd <adrian@freebsd.org>
2017-11-23 15:58:20 -0800

    Flesh out the definition of a PLSM instance, but not all the code.
    PLSM should be pretty simple - mostly just method calls as appropriate.
    
    There'll need be a PLSM implementation for say, KISS TNC, etc, to do
    "stuff" with - then we can really start having fun.

--- ax25/ax25_plsm.c
+++ ax25/ax25_plsm.c
@@ -1,47 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <err.h>
 
+#include <sys/queue.h>
+
+#include "eb.h"
 #include "ax25_plsm.h"
-
-/*
- * This implements the Physical Link State Machine.
- */
-
-/*
- * For BSD-AX25 this is a generic interface for queuing and dequeuing
- * AX.25 frames from a TNC (eg a KISS TNC) which doesn't provide a full
- * AX.25 stack.
- */
 
 struct ax25_plsm *
 ax25_plsm_create(struct ebase *eb)
 {
-	struct ax25_plsm *a;
+	struct ax25_plsm *p;
 
-	a = calloc(1, sizeof(*a));
-	if (a == NULL) {
+	p = calloc(1, sizeof(*p));
+	if (p == NULL) {
 		warn("%s: calloc", __func__);
 		return (NULL);
 	}
-	a->eb = eb;
 
-	/* here's where the queues would be created */
+	p->eb = eb;
 
-	return (a);
+	return (p);
 }
 
 void
-ax25_plsm_free(struct ax25_plsm *a)
+ax25_plsm_free(struct ax25_plsm *p)
 {
 
-	/* Free frames on the queue */
-	free(a);
+	p->owner.ph_owner_free_cb(p);
+	free(p);
 }
-
-/* PH-SEIZE request */
-/* PH-SEIZE confirm */
-/* PH-RELEASE request */
-/* PH-EXPEDITED-DATA request */
-/* PH-DATA request */
-/* PH-DATA indication */
--- ax25/ax25_plsm.h
+++ ax25/ax25_plsm.h
@@ -6,9 +6,64 @@
  *
  * It does little more than provide function pointers
  * and state pointers for an implementation of the PLSM.
+ *
+ * The owner is intended to be a link multiplexer; it will
+ * add function pointers and arg state as appropriate.
+ *
+ * The client is the relevant implementation of the physical
+ * link state.  This can be a KISS TNC, AGWPE, a 1200 baud
+ * software modem implementation, etc.
+ *
+ * The application should:
+ *
+ * + create an instance of the PLSM
+ * + create an instance of the 
  */
+
+struct ax25_plsm;
+struct ax25_pkt;
+
+typedef int ph_seize_request_t(struct ax25_plsm *);
+typedef int ph_seize_confirm_t(struct ax25_plsm *);
+typedef int ph_data_request_t(struct ax25_plsm *, struct ax25_pkt *);
+typedef int ph_release_request_t(struct ax25_plsm *);
+typedef int ph_expedited_data_request_t(struct ax25_plsm *);
+typedef int ph_data_indication_t(struct ax25_plsm *, struct ax25_pkt *);
+typedef int ph_busy_indication_t(struct ax25_plsm *);
+typedef int ph_quiet_indication_t(struct ax25_plsm *);
+typedef int ph_owner_free_t(struct ax25_plsm *);
+
+#define	AX25_PLSM_CLIENT_ARG(a)		((a)->client.arg)
+#define	AX25_PLSM_OWNER_ARG(a)		((a)->owner.arg)
+
 struct ax25_plsm {
 	struct ebase *eb;
+
+	struct {
+		void *arg;
+
+		/* AX25 methods relevant to the LMSM call methods */
+		ph_seize_request_t * ph_seize_request_cb;
+		ph_data_request_t * ph_data_request_cb;
+		ph_release_request_t * ph_release_request_cb;
+		ph_expedited_data_request_t * ph_expedited_data_request_cb;
+
+		/* extra state stuff */
+		ph_owner_free_t * ph_owner_free_cb;
+	} owner;
+
+	struct {
+		void *arg;
+
+		/* AX.25 methods relevant to the LMSM notification */
+		ph_seize_confirm_t * ph_seize_confirm_cb;
+		ph_data_indication_t * ph_data_indication_cb;
+		ph_busy_indication_t * ph_busy_indication_cb;
+		ph_quiet_indication_t * ph_quiet_indication_cb;
+	} client;
 };
+
+extern	struct ax25_plsm * ax25_plsm_create(struct ebase *eb);
+extern	void ax25_plsm_free(struct ax25_plsm *);
 
 #endif	/* __AX25_PLSM_H__ */
